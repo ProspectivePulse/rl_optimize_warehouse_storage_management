1d49c2d0521508e8cf0cdfad2e2aa34c135a2721
Nikit Sharma <sharma.nikit83@gmail.com>
2025-06-15 17:57:55 +1000

    Update setup.py
    Updated setup to include the correct project description. Also, added install requirements as a list.

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Predict the propensity of a customer to churn',
+    description='Reinforcement learning with DQN in a warehouse environment',
     author='Nikit',
     license='MIT',
+    install_requires=[
+        'torch>=2.0.0',
+        'stable-baselines3[extra]>=2.0.0',
+        'numpy',
+        'pandas',
+        'gymnasium',
+        'tensorboard',
+    ],
 )
