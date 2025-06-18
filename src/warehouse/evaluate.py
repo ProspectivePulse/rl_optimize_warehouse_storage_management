22de5935b4fff5db5c962f6e6db84fbc3f104062
Nikit Sharma <sharma.nikit83@gmail.com>
2025-06-18 17:49:27 +1000

    Update evaluate.py

--- src/warehouse/evaluate.py
+++ src/warehouse/evaluate.py
@@ -1,10 +1,24 @@
 from stable_baselines3 import DQN
 from stable_baselines3.common.vec_env import DummyVecEnv
 from warehouse.env import WarehouseEnv
+from warehouse.utils import load_config
 
 
-def evaluate_agent(model_path="models/dqn_warehouse_final.zip", episodes=5):
-    env = DummyVecEnv([lambda: WarehouseEnv()])
+def evaluate_agent():
+    config = load_config()
+
+    model_path = config["train"]["save_path"] + ".zip"  # e.g. "models/trained_agent.zip"
+    episodes = config.get("evaluate", {}).get("num_episodes", 5)
+    render = config.get("evaluate", {}).get("render", False)
+
+    env = DummyVecEnv([
+        lambda: WarehouseEnv(
+            grid_size=config["env"]["grid_size"],
+            num_items=config["env"]["num_items"],
+            max_steps=config["env"]["max_steps"]
+        )
+    ])
+    
     model = DQN.load(model_path)
 
     for ep in range(episodes):
