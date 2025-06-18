dcc39f2d8fbe3e3f436730b1a56387e9ccf16e57
Nikit Sharma <sharma.nikit83@gmail.com>
2025-06-18 17:44:19 +1000

    Update utils.py

--- src/warehouse/utils.py
+++ src/warehouse/utils.py
@@ -10,3 +10,13 @@ def set_seed(seed):
 
 def log_to_tensorboard(log_dir="logs/tensorboard"):
     return SummaryWriter(log_dir)
+
+import yaml
+import os
+
+def load_config(filename="config.yaml"):
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+    config_path = os.path.join(root_path, filename)
+    with open(config_path, "r") as file:
+        return yaml.safe_load(file)
+
